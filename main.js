ed9b5c3b8f9c37c3595e024391c2cb246f7a8a88
Tim Shea <tshea31@gmail.com>
2019-11-30 15:27:56 -0500

    updated html, css and js files

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const operators = {
     }
 };
 
-
 const operate = (operator, a, b) =>{
     let operatorKeys = Object.keys(operators);
     let selectedOperator;
@@ -22,5 +21,59 @@ const operate = (operator, a, b) =>{
             selectedOperator = operatorKeys[i];
         }
     }
-    return operators[selectedOperator](a, b);
+    result = operators[selectedOperator](a, b);
+    return result;
 }
+
+
+
+
+
+
+
+// getting both numbers
+const numberBtns = document.getElementsByClassName('number');
+
+const display = document.getElementsByClassName('calculator-screen');
+
+const displayNums = function(){
+    let x = this.value;
+    if(display[0].value.length < 10 && typeof(operator) === 'undefined' && typeof(result) === 'undefined'){
+        display[0].value = (display[0].value + x) * 1;
+        a = parseInt(display[0].value);
+    } else{
+        display[0].value = 0;
+        if(display[0].value.length < 10){
+            display[0].value = (display[0].value + x) * 1;
+            b = parseInt(display[0].value);
+            if(typeof(result) !== 'undefined'){
+                a = result;
+            }
+        }
+    }
+};
+
+const getNums = Array.from(numberBtns).forEach(function(element){
+    element.addEventListener('click', displayNums);
+});
+
+
+
+// getting the operator
+const operatorBtns = document.getElementsByClassName('operator');
+
+const operatorFunc = function(){
+    operator = this.id;
+};
+
+const getOperator = Array.from(operatorBtns).forEach(function(element){
+    element.addEventListener('click', operatorFunc);
+});
+
+
+// equals result
+const equalsBtn = document.getElementsByClassName('equal-sign');
+
+const getResult = equalsBtn[0].addEventListener('click', function(){
+    display[0].value = operate(operator, a, b);
+});
