9c98a8476cec243a60bcfe0afd02eefa56013d10
Tim Shea <tshea31@gmail.com>
2019-12-11 20:04:50 -0500

    Added a plus/minus button

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ const operate = (operator, a, b) =>{
     return (isNaN(result)) ? 'Stop that!' : result;
 }
 
-
 // number butttons
 const numberBtns = document.getElementsByClassName('number');
 
@@ -36,14 +35,18 @@ const operatorBtns = document.getElementsByClassName('operator');
 // calculator display
 const display = document.getElementsByClassName('calculator-screen');
 
-
 // display the right numbers when clicked
 let firstInput = null; 
 const getFirstInput = function(){
     let firstNum = this.value;
     if(secondInput === null && operator === null && display[0].value.length < 10){
-        display[0].value = (display[0].value + firstNum) * 1;
-        firstInput = parseFloat(display[0].value, 10);
+        if(display[0].value.includes('0.')){
+            display[0].value = display[0].value + firstNum;
+            firstInput = parseFloat(display[0].value, 10);
+        }else{
+            display[0].value = (display[0].value + firstNum) * 1;
+            firstInput = parseFloat(display[0].value, 10);
+        }
     }
 };
 
@@ -55,8 +58,14 @@ const getSecInput = function(){
         display[0].value = 0;
     }
     if(operator !== null && result === null && display[0].value.length < 10){
-        display[0].value = (display[0].value + secondNum) * 1;
-        secondInput = parseFloat(display[0].value, 10);
+        if(display[0].value.includes('0.')){
+            display[0].value = display[0].value + secondNum;
+            secondInput = parseFloat(display[0].value, 10);
+        }else{
+            display[0].value = (display[0].value + secondNum) * 1;
+            secondInput = parseFloat(display[0].value, 10);
+        }
+        
     } 
 };
 
@@ -64,9 +73,6 @@ const numberBtnArray = Array.from(numberBtns);
 const numberSelected = numberBtnArray.forEach(element =>{                           element.addEventListener('click', getFirstInput);
     element.addEventListener('click', getSecInput);
 });
-
-
-
 
 let operator = null;
 let operatorArray = [];
@@ -83,10 +89,8 @@ const operatorFunc = function(){
     }
 };
 
-
 const operatorBtnArray = Array.from(operatorBtns);
 const getOperator = operatorBtnArray.forEach(element => element.addEventListener('click', operatorFunc));
-
 
 // equals result
 const equalsBtn = document.getElementsByClassName('equal-sign');
@@ -94,7 +98,6 @@ const equalsBtn = document.getElementsByClassName('equal-sign');
 const getResult = equalsBtn[0].addEventListener('click', function(){
     display[0].value = operate(operator, firstInput, secondInput);
 });
-
 
 // clear display and variables
 const clearBtn = document.getElementsByClassName('all-clear');
@@ -107,7 +110,6 @@ const clear = clearBtn[0].addEventListener('click', function(){
     operator = null;
     decimal = null;
 });
-
 
 // delete last number input
 const deleteBtn = document.getElementsByClassName('delete');
@@ -130,7 +132,6 @@ const deleteLastInput = deleteBtn[0].addEventListener('click', function(){
     }
 });
 
-
 // insert decimal point to either input
 const decimalBtn = document.getElementsByClassName('decimal');
 
@@ -146,6 +147,25 @@ const insertDecimal = decimalBtn[0].addEventListener('click', function(){
         display[0].value = display[0].value + ('.');
     }
 });
+
+const plusMinusBtn = document.getElementsByClassName('plusMinus');
+
+const plusMinus = plusMinusBtn[0].addEventListener('click', function(){
+    if(result === null && secondInput === null && operator === null){
+        display[0].value = -(display[0].value);
+        firstInput = parseFloat(display[0].value, 10);
+    // } else if(result === null && secondInput === null && decimal < 2){
+    //     display[0].value = '0.';
+    //     secondInput = '0.';
+    } else if(operator !== null && result === null){
+        display[0].value = -(display[0].value);
+        secondInput = parseFloat(display[0].value, 10);
+    }
+});
+
+
+
+
 
 
 // const logKeyDown = function(e){
